6ca5f57e18c0703638347f2fc1718370ec8270b8
wprestonneal <mrprestonneal@gmail.com>
2021-12-19 10:16:31 -0800

    customized the volume slider and added fa icons

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # video-player
 
-a small app that features a video player on a webpage. 
+A small app that features a video player on a webpage. 
+
+---
+
+I used [iEatWebsites](https://www.youtube.com/channel/UC0o60y3FtVy3M93JcDFVreA) tutorial series on making a video player as a starting point:
+
+* [Part 1](https://www.youtube.com/watch?v=YheuzX6zmpg&t=65s)
+* [Part 2](https://www.youtube.com/watch?v=SuqEv5o0udw)
+* [Part 3](https://www.youtube.com/watch?v=5nf24no8dKU)
+
+From there I added my own custom modifications:
+
+* Font Awesome icons for play/pause/volume
+* Google font (Quicksand)
+* Different color scheme 
+* Volume slider fill based on thumb position
+* Volume slider transition
--- js/script.js
+++ js/script.js
@@ -1,41 +1,69 @@
 const videoPlayer = document.querySelector('.video-player')
 const video = videoPlayer.querySelector('.video')
 const playButton = videoPlayer.querySelector('.play-button')
+const volumeButton = videoPlayer.querySelector('.volume-button')
+const playPauseIcon = document.getElementById('play-pause-icon')
 const volume = videoPlayer.querySelector('.volume')
 const currentTimeElement = videoPlayer.querySelector('.current')
 const durationTimeElement = videoPlayer.querySelector('.duration')
 const progress = videoPlayer.querySelector('.video-progress')
 const progressBar = videoPlayer.querySelector('.video-progress-filled')
+const slider = document.getElementById('volume-slider')
+const min = slider.min
+const max = slider.max
+const value = slider.value
+const neonPurple = "rgb(194, 40, 194)"
+
+
+video.addEventListener('loadeddata', (e) => {
+    
+    if(video.readyState >= 3) {
+        let durationMinutes = Math.floor(video.duration / 60 )
+        let durationSeconds = Math.floor(video.duration - durationMinutes * 60)
+
+        durationTimeElement.innerHTML = `${durationMinutes}:${durationSeconds < 10 ? '0'+durationSeconds : durationSeconds}`
+    }
+})
+
+volumeButton.addEventListener('mouseenter', (e) => {
+    slider.style.width = "10%"
+    slider.style.visibility = "visible"
+
+    setTimeout(() => {
+        slider.style.width = "0%"
+        slider.style.visibility = "hidden"
+    }, 2000)
+})
 
 //Play and Pause button
-
-videoPlayer.addEventListener('click', (e) => {
-    if (video.paused) {
-        video.play()
-        e.target.textContent = '▌▐'
-    } else {
-        video.pause()
-        e.target.textContent = '►'
-    }  
-})
 
 playButton.addEventListener('click', (e) => {
     if (video.paused) {
         video.play()
-        e.target.textContent = '▌▐'
+        playPauseIcon.classList.add('fa-pause')
+        playPauseIcon.classList.remove('fa-play')
     } else {
         video.pause()
-        e.target.textContent = '►'
+        playPauseIcon.classList.add('fa-play')
+        playPauseIcon.classList.remove('fa-pause')
     }  
 })
 
-//volume
+// volume
 
 volume.addEventListener('mousemove', (e) => {
     video.volume = e.target.value
 })
 
+slider.style.background = `linear-gradient(to right, ${neonPurple} 0%, ${neonPurple} ${(value-min)/(max-min)*100}%, #DEE2E6 ${(value-min)/(max-min)*100}%, #DEE2E6 100%)`
+
+slider.oninput = function() {
+    this.style.background = `linear-gradient(to right, ${neonPurple} 0%, ${neonPurple} ${(this.value-this.min)/(this.max-this.min)*100}%, #DEE2E6 ${(this.value-this.min)/(this.max-this.min)*100}%, #DEE2E6 100%)`
+  };
+
 //current time and duration
+
+
 
 const currentTime = () => {
     let currentMinutes = Math.floor(video.currentTime / 60)
